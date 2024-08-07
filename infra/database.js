bc151607e375ea99ecf500755cdea4c425c74e65
Igor Baldo <109484198+IgorBaldo@users.noreply.github.com>
2024-08-07 23:54:48 +0000

    make `database.js` more robust ro error handling

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  let result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    await client.connect();
+    let result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
