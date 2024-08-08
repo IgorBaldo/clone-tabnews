6c59891656b51ad5d3c6dc43791976ce948e3840
Igor Baldo <109484198+IgorBaldo@users.noreply.github.com>
2024-08-08 22:22:50 +0000

    makes `database.js` more robust to erros

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("Credencias do Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
     await client.connect();
     let result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
