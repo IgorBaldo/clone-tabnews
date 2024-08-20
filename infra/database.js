0b65f15d38978b4329b5d49fd8299442eb970ac2
Igor Baldo <109484198+IgorBaldo@users.noreply.github.com>
2024-08-20 23:55:11 +0000

    add more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-  console.log("Credencias do Postgres: ", {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
@@ -31,3 +24,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
