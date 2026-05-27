a3a376344bddea753af94244c0b3d242746958ff
Igor Baldo <109484198+IgorBaldo@users.noreply.github.com>
2026-05-27 23:39:04 +0000

    feat: apply `authorization.filterOutput()` to all endpoints

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,15 +1,18 @@
 import { createRouter } from "next-connect";
 import database from "infra/database";
 import controller from "infra/controller.js";
+import authorization from "models/authorization.js";
 
 const router = createRouter();
 
+router.use(controller.injectAnonymousOrUser);
 router.get(getHandler);
 
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  const updateAt = new Date().toISOString();
+  const userTryingToGet = request.context.user;
+  const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
@@ -28,8 +31,8 @@ async function getHandler(request, response) {
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
-  response.status(200).json({
-    update_at: updateAt,
+  const statusObject = {
+    updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
@@ -37,5 +40,13 @@ async function getHandler(request, response) {
         opened_connections: databaseOpenedConnectionsValue,
       },
     },
-  });
+  };
+
+  const secureOutputValues = authorization.filterOutput(
+    userTryingToGet,
+    "read:status",
+    statusObject,
+  );
+
+  return response.status(200).json(secureOutputValues);
 }
